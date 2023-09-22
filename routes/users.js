cbf86ec03ea5f35db074b11a814fb4ef9418540b
Nick <421075460@qq.com>
2023-09-22 22:52:06 +0930

    2

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const bcrypt = require('bcrypt');
 // npm install bcrypt
-const saltRounds = 10;  // 指定生成salt的复杂度
+const saltRounds = 10;  
 var router = express.Router();
 const tokenManager = require('./tokenManager');
 
